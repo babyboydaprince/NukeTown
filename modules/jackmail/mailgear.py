45cc5ac8a133a361ec4c2ec0a2935bd6fc9ca136
baleta <mateusbalabenute@yahoo.com.au>
2022-04-05 19:09:17 -0300

    jackmail module and jackphone under dev

--- modules/jackmail/mailgear.py
+++ modules/jackmail/mailgear.py
@@ -4,3 +4,96 @@ import sys
 import json
 from colorama import Fore
 from getpass import getpass, getuser
+from smtplib import SMTP, SMTPAuthenticationError
+import modules.bones.crypt.sparkle as sparkle
+
+
+# Login data
+sender_email_database = 'bones/jackmail/sender.json'
+sparkle_encryption_key = getuser() + ':SPARKLE'
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+
+"""Write sender email"""
+
+
+def WriteSenderEmail():
+    username = input(
+        f'{Fore.BLUE}[?] {Fore.MAGENTA}Please'
+        ' enter sender gmail address: {Fore.BLUE}')
+    password = getpass(
+        f'{Fore.BLUE}[?] {Fore.MAGENTA}Please'
+        ' enter sender gmail password: {Fore.BLUE}')
+    server = SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    # Try login to account
+    try:
+        server.login(username, password)
+    except SMTPAuthenticationError:
+        print(
+            f'{Fore.RED}[!] {Fore.MAGENTA}Wrong'
+            ' account password, if not, try enabling this:'
+            f'\n    https://myaccount.google.com/lesssecureapps{Fore.RESET}'
+        )
+        sys.exit(1)
+    else:
+        print(
+            f'{Fore.GREEN}[+] {Fore.YELLOW}Log in Success!{Fore.RESET}'
+        )
+
+    # Save data to db?
+    confirm = input(
+        '{Fore.BLUE}[?] {Fore.MAGENTA}Whould you like to save'
+        ' this information for future reference? (y/n) : {Fore.BLUE}')
+    confirm = confirm.upper() in ('Y', 'YES')
+    if confirm:
+        # Write database
+        with open(sender_email_database, 'w') as db:
+            json.dump(
+                {
+                    'username':
+                        sparkle.Encrypt(username, sparkle_encryption_key),
+                    'password':
+                        sparkle.Encrypt(password, sparkle_encryption_key)
+                }, db
+            )
+        print(
+            f'{Fore.GREEN}[+] {Fore.YELLOW}Data saved'
+            ' to: {repr(sender_email_database)}{Fore.RESET}'
+        )
+    return [server, username]
+
+
+""" Read sender email """
+
+
+def ReadSenderEmail():
+    # Creat if it doesn't exist
+    if not os.path.exists(sender_email_database):
+        return WriteSenderEmail()
+    # Read db
+    with open(sender_email_database, 'r') as db:
+        auth = json.load(db)
+        auth['username'] = sparkle.Decrypt(
+            auth['username'], sparkle_encryption_key)
+        auth['password'] = sparkle.Decrypt(
+            auth['password'], sparkle_encryption_key)
+    # Login
+    server = SMTP(smtp_server, smtp_port)
+    server.ehlo()
+    server.starttls()
+    try:
+        server.login(
+            auth['username'],
+            auth['password']
+        )
+    except SMTPAuthenticationError:
+        print(
+            f'{Fore.RED}[!] {Fore.MAGENTA}Wrong password{Fore.RESET}'
+        )
+        os.remove(sender_email_database)
+        sys.exit(1)
+    else:
+        return [server, auth['username']]
---
+++ modules/jackphone/dataRandomizer.py
@@ -0,0 +1,15 @@
+import json
+import random
+import string
+
+mails = (
+    'mail.ru',
+    'inbox.ru',
+    'list.ru',
+    'bk.ru',
+    'ya.ru',
+    'yandex.com',
+    'yandex.ua',
+    'yandex.ru',
+    'gmail.com',
+)
