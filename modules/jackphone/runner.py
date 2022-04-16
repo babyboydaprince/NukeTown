3633f9671c339bf9895ead353ec84a0364842abc
baleta <mateusbalabenute@yahoo.com.au>
2022-04-16 18:46:54 -0300

    (add) script runner

---
+++ modules/jackphone/runner.py
@@ -0,0 +1,11 @@
+import modules.jackphone.requestSender as request
+import modules.jackphone.dataRandomizer as randomData
+
+__services = request.getServices()
+
+
+def flood(target):
+    # Get services list
+    service = randomData.random_service(__services)
+    service = request.Service(service)
+    service.sendMessage(target)
